30d9496507a126c917f4746335f13c17a26a1b1e
Shuyang Sun <sunbuffett@gmail.com>
2016-03-09 11:33:02 -0600

    Initialized Project

---
+++ MaterialDesign/MaterialDesign.h
@@ -0,0 +1,19 @@
+//
+//  MaterialDesign.h
+//  MaterialDesign
+//
+//  Created by Shuyang Sun on 3/9/16.
+//  Copyright © 2016 Shuyang Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MaterialDesign.
+FOUNDATION_EXPORT double MaterialDesignVersionNumber;
+
+//! Project version string for MaterialDesign.
+FOUNDATION_EXPORT const unsigned char MaterialDesignVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaterialDesign/PublicHeader.h>
+
+
